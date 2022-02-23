870b613c9613981637c80aa64616859281d8ab1d
SKORPION29 <sayansarkar29.slg@gmail.com>
2022-02-24 00:48:36 +0530

    twrp

--- twrp_miatoll.mk
+++ twrp_miatoll.mk
@@ -26,12 +26,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := miatoll
-PRODUCT_NAME := omni_miatoll
+PRODUCT_NAME := twrp_miatoll
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi
 PRODUCT_MANUFACTURER := xiaomi
